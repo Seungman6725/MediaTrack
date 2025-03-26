d41bcdfd324abb8882084d73d6a0c51eb33cce49
Andrew <ajeo563@aucklanduni.ac.nz>
2025-03-26 20:18:57 +1300

    Implemented re-render of page when tab of current page is clicked.:

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import Home from './pages/Home.jsx'
-import Favourites from './pages/Favourites.jsx'
-import Play from './pages/Play.jsx'
-import MyPicks from './pages/MyPicks.jsx'
+import { useLocation } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Favourites from './pages/Favourites.jsx';
+import Play from './pages/Play.jsx';
+import MyPicks from './pages/MyPicks.jsx';
 import NavBar from './components/NavBar.jsx';
-import {Routes, Route} from 'react-router-dom'
-import {FavouriteProvider} from './contexts/FavouriteContext.jsx'
-import {AddProvider} from './contexts/AddContext.jsx'
+import { Routes, Route } from 'react-router-dom';
+import { FavouriteProvider } from './contexts/FavouriteContext.jsx';
+import { AddProvider } from './contexts/AddContext.jsx';
 
-import './css/App.css'
-
+import './css/App.css';
 
 function App() {
+  const location = useLocation();
 
   return (
     <FavouriteProvider>
       <AddProvider>
-        <NavBar/>
+        <NavBar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/video-games" element={<Play />}/>
-            <Route path="/watch&play-list" element={<MyPicks />}/>
-            <Route path="/favourites" element={<Favourites />}/>
+            <Route path="/" element={<Home key={location.key} />} />
+            <Route path="/video-games" element={<Play key={location.key} />} />
+            <Route path="/watch&play-list" element={<MyPicks key={location.key} />} />
+            <Route path="/favourites" element={<Favourites key={location.key} />} />
           </Routes>
         </main>
       </AddProvider>
@@ -29,4 +30,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
